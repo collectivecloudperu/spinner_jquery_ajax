3a4e6f44e550ca16a706836fe8c73524dc6aa18f
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2020-05-15 04:08:46 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,34 @@
+$('#obtenerDatos').click(function(e) {
 
-$('#obtenerDatos').click(function() {
-                       
-	$.ajax({
-	    type: "GET",
-	    url: "json/postres.json", // Archivo con los datos 
-	    dataType: 'json',
-	    beforeSend: function() {
-	        $('#spinner').removeClass('ocultar') // Le quito la clase que oculta mi animación 
-	    },
-	    success: function(data){
-	        console.log(data); // Imprimo los datos obtenidos en la consola de Google Chrome Dev Tools
-	        let datos = "<ul class='list-group'>"; // Creo una lista '<ul>' y dentro de ella cargo elemento por elemento 
-	        for (let i = 0; i < data.length; i++) {
-	          console.log(data[i].nombre);
-	          datos += "<li class='list-group-item'>"+data[i].nombre+"</li>";
-	        }
-	      datos += "</ul>"
-	      $('#datos').html(datos); // Envío los datos obtenidos al div 'datos' que se encuentra en la vista 
-	    },
-	  complete: function(){
-	        $('#spinner').addClass('ocultar') // Oculto la animación (Spinner) de la vista para que pase a mostrarse los datos
-	    },
-	});
-  
+    e.preventDefault();
+
+    $.ajax({
+            url: "json/postres.json",
+            type: "get",
+            dataType: "json",
+            cache: false,
+            contentType: false,
+            processData: false,
+            beforeSend: function() {
+                $('#datos').html("<img src='img/spinner.gif' />");
+            },
+        })
+
+        .done(function(data) {
+
+            console.log(data);
+            let datos = "<ul class='list-group'>";
+            for (let i = 0; i < data.length; i++) {
+                console.log(data[i].nombre);
+                datos += "<li class='list-group-item'>" + data[i].nombre + "</li>";
+            }
+            datos += "</ul>"
+            $('#datos').html(datos);
+
+        })
+
+        .fail(function(data) {
+            //
+        });
+
 });
